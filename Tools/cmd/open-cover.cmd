3f1fd3448efa738e3514c620c75c92cbe36869ce
Mario Moreno <mario.moreno@live.com>
2017-02-12 22:27:26 -0300

    Add cmd files to help run common tasks
    Specifically build.cmd, open-cover.cmd to run a coverage analysis,
    vstest-console.cmd to run all unit tests and report-generator.cmd to
    generate a human friendly report from the opencover coverage result file

---
+++ Tools/cmd/open-cover.cmd
@@ -0,0 +1,43 @@
+@echo off
+SETLOCAL
+
+@REM  ----------------------------------------------------------------------------
+@REM  open-cover.cmd
+@REM
+@REM  author: mario.moreno@live.com
+@REM  ----------------------------------------------------------------------------
+
+set start_time=%time%
+set solution_dir=%CD%\..\..
+set msbuild_bin_path="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+set mstest_bin_path="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest.exe"
+set opencover_bin_path=C:\root\bin\open-cover\tools\OpenCover.Console.exe
+set opencover_proj_path=\open-cover.proj
+set solution_name=\IsoMount.sln
+
+if "%1"=="/?" goto help
+
+@REM rebuild solution
+echo %msbuild_bin_path% /m "%solution_dir%%solution_name%" /t:Rebuild /p:Configuration=Debug
+@if %errorlevel% NEQ 0 GOTO error
+
+@REM run opencover
+%msbuild_bin_path% /p:WorkingDirectory="%solution_dir%" /p:OpenCoverBinPath="%opencover_bin_path%" /p:MSTestBinPath=%mstest_bin_path% "%solution_dir%%opencover_proj_path%"
+@if %errorlevel% NEQ 0 goto error
+goto success
+
+:error
+@exit /b errorLevel
+
+:help
+echo runs a code coverage analysis using opencover 
+echo usage: open-cover.cmd [/?] 
+echo "/?" shows this help test 
+
+:success
+echo process successfully finished
+echo start time: %start_time%
+echo end time: %time%
+
+ENDLOCAL
+echo on
---
+++ Tools/cmd/report-generator.cmd
@@ -0,0 +1,45 @@
+@echo off
+SETLOCAL
+
+@REM  ----------------------------------------------------------------------------
+@REM  report-generator.cmd
+@REM
+@REM  author: mario.moreno@live.com
+@REM  ----------------------------------------------------------------------------
+
+set start_time=%time%
+set working_dir=%CD%\..\..
+set reportgenerator_bin=C:\root\bin\report-generator\tools\ReportGenerator.exe
+set opencover_file=open-cover.xml
+set target_dir=coverage-report
+
+if "%1"=="/?" goto help
+
+cd %working_dir%
+
+@rem remove previous coverate-report directory if it exists  
+IF NOT EXIST "%working_dir%\%target_dir%" GOTO NoCoverageReport
+rmdir /s /q "%target_dir%"
+:NoCoverageReport
+md "%target_dir%"
+
+rem run report generator
+"%reportgenerator_bin%" -reports:"%CD%\%opencover_file%" -targetdir:"%CD%\%target_dir%" -reporttypes:Html
+@if %errorlevel% NEQ 0 goto error
+goto success
+
+:error
+@exit /b errorLevel
+
+:help
+echo generates a html report from an open-cover.xml file 
+echo usage: report-generator.cmd [/?] 
+echo "/?" shows this help test 
+
+:success
+echo process successfully finished
+echo start time: %start_time%
+echo end time: %time%
+
+ENDLOCAL
+echo on
